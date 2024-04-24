4a96ce7f567ffb8a2f38b45d1c9d56015cf9bbdf
Maan3210 <161000615+Maan2206@users.noreply.github.com>
2024-04-24 14:06:01 +0530

    Update sketch_apr23a.ino

--- sketch_apr23a.ino
+++ sketch_apr23a.ino
@@ -10,19 +10,45 @@ void setup() {
   if (Usb.Init() == -1) {
     Serial.println("USB host shield did not initialize.");
   }
+  }
+  pinMode(motorPinLeftForward, OUTPUT);
+  pinMode(motorPinLeftBackward, OUTPUT);
+  pinMode(motorPinRightForward, OUTPUT);
+  pinMode(motorPinRightBackward, OUTPUT);  
+  
+  {
   delay(200);
-}
+  }
 
 void loop() {
   Usb.Task();
+  
   if (Xbox.XboxConnected) {
-    if (Xbox.getButtonPress(L2)) {
-      driveForward();
-    } else if (Xbox.getButtonPress(R2)) {
-      driveBackward();
+    if (Xbox.getButtonPress(L2)) { 
+      digitalWrite(motorPinLeftForward, HIGH);
+      digitalWrite(motorPinRightForward, HIGH);
+      digitalWrite(motorPinLeftBackward, LOW);
+      digitalWrite(motorPinRightBackward, LOW);
+    } else if (Xbox.getButtonPress(R2)) { 
+      digitalWrite(motorPinLeftBackward, HIGH);
+      digitalWrite(motorPinRightBackward, HIGH);
+      digitalWrite(motorPinLeftForward, LOW);
+      digitalWrite(motorPinRightForward, LOW);
+    } else if (Xbox.getButtonPress(LEFT)) { 
+      digitalWrite(motorPinLeftBackward, HIGH);
+      digitalWrite(motorPinRightForward, HIGH);
+      digitalWrite(motorPinLeftForward, LOW);
+      digitalWrite(motorPinRightBackward, LOW);
+    } else if (Xbox.getButtonPress(RIGHT)) { 
+      digitalWrite(motorPinLeftForward, HIGH);
+      digitalWrite(motorPinRightBackward, HIGH);
+      digitalWrite(motorPinLeftBackward, LOW);
+      digitalWrite(motorPinRightForward, LOW);
+    } else {               // Stop all motors
+      digitalWrite(motorPinLeftForward, LOW);
+      digitalWrite(motorPinLeftBackward, LOW);
+      digitalWrite(motorPinRightForward, LOW);
+      digitalWrite(motorPinRightBackward, LOW);
     }
   }
 }
-
-
-
